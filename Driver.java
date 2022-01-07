31d8aa3df98c9fb2938b91d8f536a16546022e2d
Anand <andymaden05@gmail,com>
2022-01-07 18:04:42 -0500

    Changed name of game object

--- Driver.java
+++ Driver.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 public class Driver {
     public static void main(String[] args) {
-        Game battleship = new Game();
+        Game gameBattleship = new Game();
       }
+
 }
